f85564c8318c8eba0483b26099981c73da162195
Lisa Hofert <hofertlisa@gmail.com>
2023-04-03 02:08:56 +0200

    main menu design

---
+++ Pong/Views/Game.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Pong.Views;
+
+public partial class Game : UserControl
+{
+    public Game()
+    {
+        InitializeComponent();
+    }
+}
